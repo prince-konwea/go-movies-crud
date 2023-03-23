4857164a63b0da58c27b222985a208fcd3763fad
prince-konwea <kokodecontee123@gmail.com>
2023-03-23 05:01:48 +0100

    completed the update movies function

--- main.go
+++ main.go
@@ -63,6 +63,22 @@ func createMovie(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(movie)
 }
 
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Contnt-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+			return
+		}
+	}
+}
+
 func main() {
 	r := mux.NewRouter()
 	movies = append(movies, Movie{ID: "1", Isbn: "433227", Title: "John wick", Director: &Director{FirstName: "Prince", LastName: "Konwea"}})
@@ -71,7 +87,7 @@ func main() {
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
 	r.HandleFunc("/movies/{id}", updateMovie).Methods("Put")
-	r.HandleFunc("/movies/{id}", deleteMovie).Mthodds("DELETE")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
 	fmt.Printf("Starting server at port 8000")
 	log.Fatal(http.ListenAndServe(":8000", r))
